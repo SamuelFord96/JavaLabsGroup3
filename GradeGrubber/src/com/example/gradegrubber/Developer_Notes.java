c18993350c073035cc48ac6d733f3c9352d8a877
Pyxylation <Pyxylation@outlook.com>
2016-11-21 21:38:06 -0500

    Developer Notes
    Added a class just for our personal notes so we can have list of what
    needs to be done and what is done. This is beyond what comments should
    go in each class of what were doing.

---
+++ GradeGrubber/src/com/example/gradegrubber/Developer_Notes.java
@@ -0,0 +1,32 @@
+package com.example.gradegrubber;
+
+public class Developer_Notes {
+	/*This is the location where we can make notes to each other that go deeper than comments inside the code. 
+	 * 		Make sure you say who you are, so we know who said what
+	 *
+	 *Must have features of the app:
+	 *	1) Calculation of each assignment
+	 *	2) Calculation of each section of grades (quiz, test, ect)
+	 *	3) Calculation of total grade
+	 *	4) Ability to add classes and take them away
+	 *	5) Main view: Grades next to the classes
+	 *	6) Grade view: custom assignment type
+	 *	7) Back buttons
+	 *	8) Scrolling for the entire screen, rather than mini-scrolls for each assignment type
+	 *	9) Basic settings page
+	 * 
+	 * Should have features:
+	 * 	1) Grade Glow
+	 * 	2) Press and hold to edit stuff
+	 * 	3) Future grade calculator
+	 * 	4) Custom buttons and text
+	 * 
+	 * Nice to have features:
+	 * 	1) Theme change
+	 * 	2) Colors for each class
+	 * 	3) Auto-fill points for each assignment
+	 * 	4) Custom colors for grade glow
+	 * 	5) Settings: Take out the add class button
+	 */
+
+}
