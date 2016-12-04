fbfda448ddcecac223f3956acaef3e50f97394d6
Pyxylation <Pyxylation@outlook.com>
2016-12-04 18:31:09 -0500

    Deleted some stuffs... I guess

--- Checkbook-ArrayListGrp3/gen/com/mis4800/group3/checkbook_android/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.mis4800.group3.checkbook_android;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
