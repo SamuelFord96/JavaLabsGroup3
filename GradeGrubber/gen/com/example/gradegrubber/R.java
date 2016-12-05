204a5ce89ca50159e4adb3e72df1a006e28bb88b
Pyxylation <Pyxylation@outlook.com>
2016-12-04 20:17:06 -0500

    Course
    Made basic change to the course activity

--- GradeGrubber/gen/com/example/gradegrubber/R.java
+++ GradeGrubber/gen/com/example/gradegrubber/R.java
@@ -29,11 +29,11 @@ public final class R {
         public static final int addAssingmentName=0x7f090004;
         public static final int btnAddCourse=0x7f090009;
         public static final int buttonAddAssingment=0x7f090001;
+        public static final int classGrade=0x7f090006;
         public static final int lstAssignmentType1=0x7f090000;
         public static final int lstYourCourses=0x7f09000a;
         public static final int maxPoints=0x7f090002;
         public static final int pointsAchieved=0x7f090003;
-        public static final int textView1=0x7f090006;
         public static final int tvCourseTitle=0x7f090005;
         public static final int tvTitleGrubberHubber=0x7f090007;
         public static final int tvYourCourses=0x7f090008;
