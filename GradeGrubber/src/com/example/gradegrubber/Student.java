5266de58046e0913b5064efb873056bbf711b601
Samuel Ford <ford.136@wright.edu>
2016-12-04 20:59:22 -0500

    Pretty much finished Student Class
    Added methods and variables

--- GradeGrubber/src/com/example/gradegrubber/Student.java
+++ GradeGrubber/src/com/example/gradegrubber/Student.java
@@ -24,5 +24,29 @@ public class Student {
 	public void addCourse(Course newCourse){
 		myCourses.add(newCourse);
 	}
-	
+	public void deleteCourse(int positionOfCourse){
+		myCourses.remove(positionOfCourse);
+	}
+	public void updateCourse(int positionOfCourse, Course newCourse){
+		myCourses.set(positionOfCourse, newCourse);
+	}
+	/**
+	 * @return the myName
+	 */
+	public String getMyName() {
+		return myName;
+	}
+	/**
+	 * @param myName the myName to set
+	 */
+	public void setMyName(String myName) {
+		this.myName = myName;
+	}
+	/**
+	 * @return the myCourses
+	 */
+	public List<Course> getMyCourses() {
+		return myCourses;
+	}
+
 }
